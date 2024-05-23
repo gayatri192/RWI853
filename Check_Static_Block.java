04c646922ec40c335023ea84e2c754d458426307
Gayatri Lohar <103237584+gayatri192@users.noreply.github.com>
2024-05-23 18:48:54 +0530

    Add files via upload

---
+++ Check_Static_Block.java
@@ -0,0 +1,10 @@
+public class Check_Static_Block {
+    // Static block
+    static {
+        System.out.println("Static Block: Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main Method: Hello, World!");
+    }
+}
