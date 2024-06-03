e7bfc441ac9b6df7bd928e81f049f6bb15af613b
Gayatri Lohar <103237584+gayatri192@users.noreply.github.com>
2024-06-03 17:16:44 +0530

    Add files via upload

---
+++ AbstractConcreteMethod/src/Dog.java
@@ -0,0 +1,8 @@
+class Dog extends Animal {
+
+  // provide implementation of abstract method
+  public void makeSound() {
+    System.out.println("Bark bark");
+  }
+}
+
---
+++ AbstractConcreteMethod/src/Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+
+    // create an object of Dog class
+    Dog d1 = new Dog();
+
+    d1.makeSound();
+    d1.eat();
+  }
+}
