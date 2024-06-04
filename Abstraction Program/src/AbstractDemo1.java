b43fb96356fdff594c2b840b44aacde57bd513b9
Gayatri Lohar <103237584+gayatri192@users.noreply.github.com>
2024-06-04 15:43:30 +0530

    Add files via upload

---
+++ Abstraction Program/src/AbstractDemo1.java
@@ -0,0 +1,19 @@
+
+public class AbstractDemo1 extends AbstractDemo
+{
+	int a=10,b=20,c;
+	void sum()
+	{
+	c=a+b;
+	System.out.println("The sum is: "+c);
+	}
+
+	void sub()
+	{
+	c=a-b;
+	System.out.println("The Subtraction is: "+c);
+	}
+	
+
+
+}
