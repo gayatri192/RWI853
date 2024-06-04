53b7253d9f0cc6910ea39b1766e196d2a3bb83ef
Gayatri Lohar <103237584+gayatri192@users.noreply.github.com>
2024-06-04 15:39:54 +0530

    Add files via upload

---
+++ Abstraction Program/src/Abstract.java
@@ -0,0 +1,17 @@
+
+public class Abstract {
+	
+
+		public static void main(String[] args)
+		{
+			
+			AbstractDemo1 D1=new AbstractDemo1();
+			D1.sum();
+			D1.sub();
+
+		}
+
+}
+
+
+
---
+++ Abstraction Program/src/AbstractDemo.java
@@ -0,0 +1,9 @@
+
+abstract class AbstractDemo {
+	
+	abstract void sum();
+	abstract void sub();
+	
+	
+
+}
